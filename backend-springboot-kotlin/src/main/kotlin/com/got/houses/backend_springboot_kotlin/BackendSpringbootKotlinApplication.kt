cf64cf94c08ab81975847a61d471feb344b3e27c
Danielg1406 <danielgomeza1406@gmail.com>
2024-07-30 21:50:53 +0200

    feature backend: create backend base

---
+++ backend-springboot-kotlin/src/main/kotlin/com/got/houses/backend_springboot_kotlin/BackendSpringbootKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.got.houses.backend_springboot_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendSpringbootKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendSpringbootKotlinApplication>(*args)
+}
---
+++ backend-springboot-kotlin/src/test/kotlin/com/got/houses/backend_springboot_kotlin/BackendSpringbootKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.got.houses.backend_springboot_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendSpringbootKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
