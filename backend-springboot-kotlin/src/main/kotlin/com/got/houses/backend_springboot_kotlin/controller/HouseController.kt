b3433295290a25b08811b3e2741312041fea9bcd
Danielg1406 <danielgomeza1406@gmail.com>
2024-07-30 22:26:04 +0200

    feature controller: creation controller

---
+++ backend-springboot-kotlin/src/main/kotlin/com/got/houses/backend_springboot_kotlin/controller/HouseController.kt
@@ -0,0 +1,16 @@
+package com.got.houses.backend_springboot_kotlin.controller
+
+import com.got.houses.backend_springboot_kotlin.service.HouseService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HouseController(private val houseService: HouseService) {
+    @GetMapping("/houses")
+    fun searchHouses(@RequestParam query: String) = houseService.searchHouses(query)
+
+    @GetMapping("/houses/{id}")
+    fun getHouseDetails(@PathVariable id: Long) = houseService.getHouseDetails(id)
+}
